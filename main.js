0c04248f55f1227fc9e0dd26dd75f7bb6c3ea112
modarsh99 <modarwork@gmail.com>
2024-09-15 20:45:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const social = document.querySelector(".social");
-const socialopen = document.querySelector("#logo-socilao");
-const logoout = document.querySelector(".logo-bmw");
-const socialclose = document.querySelector("#logo-socilac");
-socialopen.addEventListener('click',function(){
-    socialopen.style.display = "none"
-    socialclose.style.display = "inline"
-    social.style.display = "flex"
-    
+document.querySelector('.mobail-bar-list-opean').addEventListener('click',function(){
+    document.querySelector('.mobile-list').style.cssText = "transition: 0.4s;translate: 0px 0px;"
 })
-socialclose.addEventListener('click',function(){
-    socialopen.style.display = "inline"
-    socialclose.style.display = "none"
-    social.style.display = "none"
+document.querySelector('.mobail-bar-list-clouse').addEventListener('click',function(){
+    document.querySelector('.mobile-list').style.cssText = "transition: 1s;translate: 0px -1000px;"
+})
+document.querySelector('#open-social').addEventListener('click',function(){
+    document.querySelector('#open-social').style.cssText = "scale: 0.0001;transition: 1s;"
+    document.querySelector('.social-media-icon').style.cssText = "transition: 1s;translate: 0px 0px;"
 })
 
+document.querySelector('#close-social').addEventListener('click',function(){
+    document.querySelector('.social-media-icon').style.cssText = "transition: 1s;translate: -100px 0px;"
+    document.querySelector('#open-social').style.cssText = "scale: 1;transition: 1s;"
 
-
+})
